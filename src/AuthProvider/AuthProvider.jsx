514119438a44f936f6a535e18f287b0e9da19d38
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-02 03:15:06 +0600

    login and registation form done

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,19 +1,37 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import React, { createContext } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import app from "../firebase.config";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCeDEGsPSNWXlOlcrl-7qii-O48w2DyESk",
-  authDomain: "chef-recipe-hunter-clien-7005f.firebaseapp.com",
-  projectId: "chef-recipe-hunter-clien-7005f",
-  storageBucket: "chef-recipe-hunter-clien-7005f.appspot.com",
-  messagingSenderId: "913406490345",
-  appId: "1:913406490345:web:ae9d784d20d278bcb76278",
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, provider);
+  };
+  const authInfo = {
+    createUser,
+    loginUser,
+    signInWithGoogle,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export default app;
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Register from "./components/Register.jsx";
 import Blog from "./components/Blog.jsx";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -40,6 +41,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
