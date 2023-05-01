87c6c7f310c445d9639e08de376a78689f464f64
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-02 01:04:08 +0600

    react route set

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,38 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Header from "./components/Header.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Blog from "./components/Blog.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header></Header>,
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+    ],
   },
 ]);
 
