679280fcdf82e1bfafcbc4d15db567413e51ab96
Shaik Abdul Ahad <118795198+ShaikAbdul01@users.noreply.github.com>
2023-05-03 03:32:40 +0600

    toast added

--- src/components/Pages/Recipe.jsx
+++ src/components/Pages/Recipe.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import Rating from "react-rating";
+import { BsBookmark, BsBookmarkFill, BsStar, BsStarFill } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Recipe = ({ recipe }) => {
   const [fold, setFold] = useState(true);
-  //   console.log(recipe);
+  const [bookmark, setBookmark] = useState(true);
   const { name, cooking_method, rating, ingredients } = recipe;
+  //   console.log(recipe);
+
+  const handleClick = () => {
+    toast.success("Your favourite recipe added!!");
+  };
   return (
     <div className="bg-gray-100 p-6 rounded shadow-lg">
       <p className="mb-2 text-xl font-bold leading-none sm:text-2xl">{name}</p>
@@ -34,7 +43,41 @@ const Recipe = ({ recipe }) => {
           </span>
         </>
       )}
-      <p className="text-gray-700 font-bold">Rating: {rating}</p>
+
+      {/*  <p className="text-gray-700 font-bold mt-4 mb-2">
+        Rating</p> */}
+      <div className="flex justify-between items-center mt-4">
+        <div className="">
+          <Rating
+            className="mr-2"
+            placeholderRating={rating}
+            emptySymbol={<BsStar></BsStar>}
+            placeholderSymbol={
+              <BsStarFill className="text-warning"></BsStarFill>
+            }
+            fullSymbol={<BsStarFill></BsStarFill>}
+          />
+          {rating}
+        </div>
+        <div className="text-xl">
+          {bookmark ? (
+            <span
+              onClick={() => setBookmark(!bookmark) || handleClick()}
+              className="text-blue-600"
+            >
+              <BsBookmark></BsBookmark>
+              <ToastContainer />
+            </span>
+          ) : (
+            <span
+              onClick={() => setBookmark(!bookmark)}
+              className="text-blue-600"
+            >
+              <BsBookmarkFill />
+            </span>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
